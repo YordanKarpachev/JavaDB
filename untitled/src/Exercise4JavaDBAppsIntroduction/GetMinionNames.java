67777c7ad05614cdadee9430327e689c42389584
YordanKarpachev <yordkarp@icloud.com>
2023-01-17 19:09:18 +0100

    3. Get Minion Names Write a program that prints on the console all minion names and their age for given villain id. For the output, use the formats given in the examples.

---
+++ untitled/src/Exercise4JavaDBAppsIntroduction/GetMinionNames.java
@@ -0,0 +1,45 @@
+package Exercise4JavaDBAppsIntroduction;
+
+import java.sql.*;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class GetMinionNames {
+    public static void main(String[] args) throws SQLException {
+        Properties properties = new Properties();
+        properties.setProperty("user", "root");
+        properties.setProperty("password", "123456");
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/minions_db", properties);
+
+
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+
+        PreparedStatement resultSet1 = connection.prepareStatement("select v.name from minions_villains " +
+                "         join villains v on v.id = minions_villains.villain_id " +
+                "where villain_id = ?;");
+        resultSet1.setInt(1, n);
+        ResultSet res = resultSet1.executeQuery();
+
+        if (!res.next()) {
+            System.out.println("No villain with ID 10 exists in the database.");
+            connection.close();
+            return;
+        }
+
+        System.out.println("Villian: " + res.getString("name"));
+
+        PreparedStatement statement = connection.prepareStatement("select  m.name, m.age from minions_villains " +
+                "join minions m on m.id = minions_villains.minion_id " +
+                "                      where villain_id = ?");
+        statement.setInt(1, n);
+        ResultSet resultSet = statement.executeQuery();
+
+        for (int i = 1; resultSet.next(); i++) {
+
+            System.out.println(i + ". " + resultSet.getString("name") + resultSet.getInt("age"));
+        }
+    }
+}
