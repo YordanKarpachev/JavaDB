f83f1c86fa478c6e08660d83ff11127ddd914677
YordanKarpachev <yordkarp@icloud.com>
2023-01-17 19:09:54 +0100

    2. Get Villains’ Names Write a program that prints on the console all villains’ names and their number of minions. Get only the villains who have more than 15 minions. Order them by number of minions in descending order.

--- untitled/src/Exercise4JavaDBAppsIntroduction/GetVillainsNames.java
+++ untitled/src/Exercise4JavaDBAppsIntroduction/GetVillainsNames.java
@@ -5,16 +5,17 @@ import java.util.Properties;
 
 
 public class GetVillainsNames {
+
     public static void main(String[] args) throws SQLException {
 
 
       Connection sqlConnection = Utils.getSqlConnection();
         PreparedStatement statement = sqlConnection.prepareStatement(
                 "select  v.name , count( distinct mv.minion_id) minions_count from " +
-                "villains v join minions_villains mv on v.id = mv.villain_id " +
-                "group by  mv.villain_id " +
-                "having minions_count > 15 " +
-                "order by minions_count;");
+                        "villains v join minions_villains mv on v.id = mv.villain_id " +
+                        "group by  mv.villain_id " +
+                        "having minions_count > 15 " +
+                        "order by minions_count;");
 
         ResultSet resultSet = statement.executeQuery();
 
