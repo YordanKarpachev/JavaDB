41effb836ce48aa5975938ccc28ca6cbcc9692bf
YordanKarpachev <yordkarp@icloud.com>
2023-03-06 04:15:13 +0100

    Exercise: Hibernate Code First

---
+++ ExerciseHibernateCodeFirst/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManager codeFirst = Persistence.createEntityManagerFactory("CodeFirst").createEntityManager();
+        codeFirst.getTransaction().begin();
+
+
+
+        codeFirst.getTransaction().commit();
+        codeFirst.close();
+    }
+}
