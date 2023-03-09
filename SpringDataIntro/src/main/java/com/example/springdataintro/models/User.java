44af310fd36d35a03bc6565cce5fb8f64e2fdb55
YordanKarpachev <yordkarp@icloud.com>
2023-03-09 01:14:16 +0100

    Project Setup

--- SpringDataIntro/src/main/java/com/example/springdataintro/models/User.java
+++ SpringDataIntro/src/main/java/com/example/springdataintro/models/User.java
@@ -1,4 +1,25 @@
 package com.example.springdataintro.models;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private int age;
+
+    @OneToMany
+    private List<Account> accounts;
+
+
 }
